fb7e0adb4658e8e095839baaad63ae1e66a31be6
pdiakubama <pdiakubama@seattleu.edu>
2024-04-21 12:51:07 -0700

    Code cleanup

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:bmi_calculator/components/icon_content.dart';
-import 'dart:ffi';
 import 'package:bmi_calculator/constants.dart';
 import 'package:bmi_calculator/components/round_icon_button.dart';
 import 'package:bmi_calculator/screens/results_page.dart';
